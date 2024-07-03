874aad5528768ebbcda4bb7644d47fa02efc7e80
Sahilchouhan20 <sahilchouhan0103@gmail.com>
2024-07-03 12:26:12 +0530

    add on task 1

--- Hash.rb
+++ Hash.rb
@@ -1,11 +1,19 @@
 #Create a hash named person with keys :name, :age, and :city and corresponding values "John", 25, and "New York". Write a Ruby program to print the value of each key in the hash.
-Person={
-  "name"=>"Jhon",
-  "age"=> 25,
-  "city"=>"New York"
-}
+class HashPerson
+  def initialize
+    @Person={
+      "name"=>"Jhon",
+      "age"=> 25,
+      "city"=>"New York"
+    }
+  end
 
-
-Person.each do|key,value|
-  puts"Person's keys and value is: #{key} #{value}"
+  def displayDetails
+    @Person.each do|key,value|
+      puts"Person's keys and value is: #{key} #{value}"
+    end
+  end
 end
+
+a=HashPerson.new
+a.displayDetails
